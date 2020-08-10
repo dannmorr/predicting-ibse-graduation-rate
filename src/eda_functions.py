ee4df3f21389856b8da0f168c64b984cdde6a44b
dannmorr <dannmorr@gmail.com>
2020-08-09 20:02:44 -0500

    modeling

---
+++ src/eda_functions.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.formula.api as smf
+
+
+
+def clean_col(df):
+
+    '''
+    clean columns
+    '''
+    df.columns = [x.lower()
+                 .replace("-", " ")
+                 .replace("  ", " ")
+                 .replace(" ", "_")
+                 .replace("__", "_")
+                 .rstrip()
+                 .replace("#", "number")
+                 .replace("%", "percent") for x in df.columns]
+    return df.columns.to_list()
+
+
+
+
+def proj_eda(df): 
+        
+    '''
+    EDA functions
+    CITE
+    '''
+    eda_df = {}
+    eda_df['null_sum'] = df.isnull().sum()
+    eda_df['null_pct'] = df.isnull().mean()
+    eda_df['dtypes'] = df.dtypes
+    eda_df['count'] = df.count()
+    eda_df['mean'] = df.mean()
+    eda_df['median'] = df.median()
+    eda_df['min'] = df.min()
+    eda_df['max'] = df.max()
+    
+    return pd.DataFrame(eda_df)
+
+
+
+
+
+def heatmap(df, dependent_variable):
+    '''
+    Takes df, a dependant variable as str
+    Returns a heatmap of all independent variables' correlations with dependent variable 
+    '''
+
+    plt.figure(figsize=(10, 10))
+    g = sns.heatmap(df.corr()[[dependent_variable]].sort_values(by=dependent_variable), 
+                    annot=True, 
+                    cmap='YlGnBu', 
+                    cbar=False,
+                    vmin=-1,
+                    vmax=1)  
+    return g
+
+
+
+
+def forward_selected(data, response):
+    """Linear model designed by forward selection.
+
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    """
+    remaining = set(data.columns)
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            formula = "{} ~ {} + 1".format(response,
+                                           ' + '.join(selected + [candidate]))
+            score = smf.ols(formula, data).fit().rsquared
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model
